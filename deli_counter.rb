d187c85a5b2ab13dd21441a7ad2118b0b07c1c64
Dave Winniford <dwinniford@gmail.com>
2020-01-22 22:49:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,12 @@
-def line(restaurant)
-  if restaurant.size == 0 
+def line(katz_deli)
+  if katz_deli.size == 0 
     puts "The line is currently empty."
   end 
+  else
+    list = ""
+    katz_deli.each_with_index do  |name, i| 
+      list << " #{i + 1}. #{name}" 
+      end 
+    puts "The line is currently:#{list}" 
 end 
+
